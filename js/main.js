25d5baf3daee70c887e98020bed0cbb09c56591e
Aleksandr Nikitin <a.nikitin@data357.com>
2014-05-13 13:01:43 +0300

    Duty add done page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
 (function(){
-	var $selectForm;
+	var $selectForm,
+		$closeDialog,
+		$bgDialog;
 
 	$(function(){
 		$selectForm = $('.select-form');
+		$closeDialog = $('.close-dialog');
+		$bgDialog = $('.bg-dialog');
 
 		$selectForm.on('change', 'select', function(){
 			var $span = $(this).closest('.select-form').find('span');
 			$span.text($(this).val());
 		});
+
+		$closeDialog.on('click', function(e){
+			var elem = e.target || e.srcElement;
+			if ($(elem).hasClass('close-dialog')){
+				closeDialog();
+			}
+		});
 	});
+
+	function closeDialog() {
+		$('.dialog-window').addClass('_display_none');
+	}
 })();
